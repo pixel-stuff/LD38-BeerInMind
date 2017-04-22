afba17b715f3e6a746a064ef540b495c9bd6c1df
mathias <mathias@marbotic.fr>
2017-04-22 18:34:33 +0200

    Creation de tous les Event de transition

---
+++ Assets/Script/Events/Event.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.EventSystems;
+
+public class Event : MonoBehaviour {
+
+	public static Action m_mainTrigger;
+
+	public void OnMouseUp()
+	{
+		if (m_mainTrigger != null) {
+			m_mainTrigger ();
+		}
+	}
+}
