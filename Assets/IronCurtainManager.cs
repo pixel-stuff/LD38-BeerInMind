7c55d6f5cbe3cee014995adebd859e970055a1f0
mathias <mathias@marbotic.fr>
2017-04-23 21:18:54 +0200

    curtain Manager

--- Assets/IronCurtainManager.cs
+++ Assets/IronCurtainManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class IronCurtainManager : MonoBehaviour {
 
 	public static IronCurtainManager m_instance;
+	public Text m_textExplicatif;
+	public Text m_buttonText;
+
 	void Awake(){
 		if(m_instance == null){
 			m_instance = this;
@@ -24,13 +29,34 @@ public class IronCurtainManager : MonoBehaviour {
 
 	public void EndTheDay(){
 		GameStateManager.setGameState (GameState.EndOfTheDay);
+		TimeManager.timePlay = false;
+		this.GetComponent<Animation> ().Play ("CurtainApparition");
+	}
+
+	public void StartNextDay(){
+		GameStateManager.setGameState (GameState.Playing);
+		TimeManager.timePlay = true;
+		this.GetComponent<Animation> ().Play ("CurtainRemove");
 	}
 
 	public void SetGameOver(string message){
+		GameStateManager.setGameState (GameState.GameOver);
+		TimeManager.timePlay = false;
+		this.GetComponent<Animation> ().Play ("CurtainApparition");
+	}
+
+	public void ButtonClick(){
+		if (GameStateManager.getGameState () == GameState.EndOfTheDay) {
+			StartNextDay ();
+		} else if (GameStateManager.getGameState () == GameState.EndOfTheDay) {
+			GameStateManager.setGameState (GameState.Playing);
+			SceneManager.LoadScene ("levelScene");
+		}
 	}
 
 	public void OnDestroy(){
 		TimeManager.m_DayEnding -= EndTheDay;
 	}
+
 
 }
