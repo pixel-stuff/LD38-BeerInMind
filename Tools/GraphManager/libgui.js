80fb35c411de46c5aba2c89c1f1c2ab19644bcf8
pplans <pierre.plans@gmail.com>
2017-04-23 16:36:24 +0200

    up Graph Editor

--- Tools/GraphManager/libgui.js
+++ Tools/GraphManager/libgui.js
@@ -1,13 +1,13 @@
 function libgui_build_combobox(id, data)
 {
 	var buffer = "";
-	buffer+="<select id=\""+id+"\" value=\""+data.defaultvalue+"\">";
+	buffer+="<select id=\""+id+"\" name=\""+id+"\">";
 	for(var i=0;i<data.values.length;i++)
 	{
 		var selected = "";
 		if (data.values[i].value==data.defaultvalue)
-			selected = " selected";
-		buffer+="<option value=\""+data.values[i].value+"\""+selected+">"+data.values[i].label+"</option>";
+			selected = " selected=\"selected\"";
+		buffer+="<option"+selected+" value=\""+data.values[i].value+"\">"+data.values[i].label+"</option>";
 	}
 	buffer+="</select>";
 	return buffer;
