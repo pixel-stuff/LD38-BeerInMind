07e5c799d5282f0d9e8f42e55a4aeee8acac224a
mathias <mathias@marbotic.fr>
2017-04-23 12:54:31 +0200

    Click Click Click

--- Assets/Script/Events/TVEvent.cs
+++ Assets/Script/Events/TVEvent.cs
@@ -1,6 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class TVEvent : Event {
+public class TVEvent : MonoBehaviour {
+	public static Action<bool> m_mainTrigger;
+	private bool m_tvIsOn = false;
+
+	public void OnMouseUp()
+	{
+		m_tvIsOn = !m_tvIsOn;
+		if (m_mainTrigger != null) {
+			m_mainTrigger (m_tvIsOn);
+		}
+	}
 }
