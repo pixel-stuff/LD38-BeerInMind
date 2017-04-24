0c751ca43618965b01f4e2b43915d7a0d78b3263
mathias <mathias@marbotic.fr>
2017-04-24 16:46:16 +0200

    Correction click alors qu'élément en premier plan visible

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -324,6 +324,9 @@ public class Character : MonoBehaviour {
 
     public void OnMouseUp()
     {
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate)
+			return;
+		
         if (m_isWaitingForClick)
         {
             m_isWaitingForClick = false;
