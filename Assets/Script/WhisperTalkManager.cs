80b41275c30f4c1786d1b3cc955b688b68a7768e
mathias <mathias@marbotic.fr>
2017-04-24 12:36:32 +0200

    bubulle brigitte

--- Assets/Script/WhisperTalkManager.cs
+++ Assets/Script/WhisperTalkManager.cs
@@ -6,6 +6,7 @@ using System;
 public class WhisperTalkManager : MonoBehaviour {
 
 	public TextMesh m_text;
+	public GameObject m_container;
 	public int m_tickAlive = 3;
 	private int m_tickBeforeErase = 0;
 
@@ -13,30 +14,32 @@ public class WhisperTalkManager : MonoBehaviour {
 	// Use this for initialization
 	void Awake () {
 		StopDisplayWhisper ();
+		StartDisplayWhisper ("bfsbrv",true);
 		TimeManager.OnTicTriggered += TickHappen;
 	}
 
 	public void StartDisplayWhisper(string txt, bool displayOnRight = true){
-		m_text.gameObject.SetActive(true);
-		this.GetComponent<SpriteRenderer> ().enabled = true;
+		Debug.Log ("");
 		m_text.text = txt;
 		m_tickBeforeErase = m_tickAlive;
+		m_container.SetActive (true);
 		if (!displayOnRight) {
-			this.transform.localRotation = Quaternion.identity;
+			m_container.transform.localRotation = Quaternion.identity;
 			m_text.transform.localRotation = Quaternion.identity;
-			Vector3 pos = this.transform.localPosition;
-			pos.x = -pos.x;
-			this.transform.localPosition = pos;
-			pos = m_text.transform.localPosition;
-			pos.x = -pos.x;
-			m_text.transform.localPosition = pos;
+			m_container.transform.localPosition = new Vector3 (-4.18f,0.39f,0.0f);
+		} else {
+			m_container.transform.localPosition = new Vector3 (-0.42f,0.39f,0.0f);
+			Vector3 rot = m_container.transform.rotation.eulerAngles;
+			rot = new Vector3(rot.x,-180f,rot.z);
+			m_container.transform.rotation = Quaternion.Euler(rot);
+			rot = new Vector3(rot.x,180f,rot.z);
+			m_text.transform.rotation = Quaternion.Euler(rot);
 		}
 	}
 
 	public void StopDisplayWhisper(){
-		m_text.gameObject.SetActive(false);
-		this.GetComponent<SpriteRenderer> ().enabled = false;
 		m_text.text = "";
+		m_container.SetActive (false);
 	}
 
 	public void TickHappen(GameTime gt){
