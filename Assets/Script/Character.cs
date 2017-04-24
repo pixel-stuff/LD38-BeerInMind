2a5f4086be0dce1a19c7ce36acb1eee2cc3777fd
pplans <pierre.plans@gmail.com>
2017-04-24 16:29:54 +0200

    Add debug info label from nodes

--- Assets/Script/Character.cs
+++ Assets/Script/Character.cs
@@ -64,6 +64,7 @@ public class Character : MonoBehaviour {
 			mood = (Node.eMood)System.Enum.Parse (typeof(Node.eMood), _node.mood, true);
 		}
         return new Node(
+            _node.label,
             day,
             hour,
             minut,
