80aebb1c8d4b5ca4c8951762ae9cc922c8da93b8
jmorel <jmorel@betomorrow.com>
2017-04-25 22:27:11 +0200

    buble barman have cursor

---
+++ Assets/BarmanBubble.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarmanBubble : MonoBehaviour {
+
+	public Sprite m_hover;
+	public Sprite m_clic;
+
+	public void OnClick()
+	{
+
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate)
+			return;
+
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+
+	public void OnOver()
+	{
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate)
+			return;
+
+		Cursor.SetCursor (m_hover.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+	public void OnNoOver()
+	{
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
+	}
+}
