2d756b1b8374eb93d0dcc6c122048468b97d92f9
mathias <mathias@marbotic.fr>
2017-04-25 02:14:59 +0200

    sqvs

--- Assets/Script/Events/KeysEvent.cs
+++ Assets/Script/Events/KeysEvent.cs
@@ -6,6 +6,8 @@ using System;
 
 public class KeysEvent : MonoBehaviour {
 	public static Action m_mainTrigger;
+	public Sprite m_hover;
+	public Sprite m_clic;
 
 	public void OnMouseUp()
 	{
@@ -21,5 +23,29 @@ public class KeysEvent : MonoBehaviour {
 		}
 		this.GetComponent<SpriteRenderer> ().enabled = false;
 		this.GetComponent<BoxCollider2D> ().enabled = false;
+		Cursor.SetCursor (m_hover.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+	public void OnMouseDown()
+	{
+
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate || BarmanManager.m_instance.m_isActive)
+			return;
+
+		Cursor.SetCursor (m_clic.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+
+	void OnMouseEnter()
+	{
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate || BarmanManager.m_instance.m_isActive)
+			return;
+
+		Cursor.SetCursor (m_hover.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+	void OnMouseExit()
+	{
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
 	}
 }
