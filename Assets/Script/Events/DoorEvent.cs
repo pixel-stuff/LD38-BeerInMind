3cb62c310a14de30196217b5249829f4312078ba
jmorel <jmorel@betomorrow.com>
2017-04-25 14:00:40 +0200

    door with interaction feedback

--- Assets/Script/Events/DoorEvent.cs
+++ Assets/Script/Events/DoorEvent.cs
@@ -5,11 +5,13 @@ using System;
 
 public class DoorEvent : MonoBehaviour {
 	public static Action m_mainTrigger;
+	public Sprite m_hover;
+	public Sprite m_clic;
 
 	public void OnMouseUp()
 	{
 		//Debug.Log (MainTalkManager.m_instance.m_isActivate + " / " + UIClickManager.m_instance.m_isActivate + " / " + IronCurtainManager.m_instance.m_isActivate);
-		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate)
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate || BarmanManager.m_instance.m_isActive)
 			return;
 
 		if (m_mainTrigger != null) {
@@ -18,5 +20,28 @@ public class DoorEvent : MonoBehaviour {
 		if (this.GetComponent<AudioSource> () != null) {
 			this.GetComponent<AudioSource> ().Play ();
 		}
+	}
+
+	public void OnMouseDown()
+	{
+
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate || BarmanManager.m_instance.m_isActive)
+			return;
+
+		Cursor.SetCursor (m_clic.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+
+	void OnMouseEnter()
+	{
+		if (MainTalkManager.m_instance.m_isActivate || UIClickManager.m_instance.m_isActivate || IronCurtainManager.m_instance.m_isActivate || BarmanManager.m_instance.m_isActive)
+			return;
+
+		Cursor.SetCursor (m_hover.texture, Vector2.zero, CursorMode.ForceSoftware);
+	}
+
+	void OnMouseExit()
+	{
+		Cursor.SetCursor(null, Vector2.zero, CursorMode.ForceSoftware);
 	}
 }
